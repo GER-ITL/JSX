3f5a7c12135b0183c035b6430d5dc1bcef6c8290
GER-ITL <danteryzvelt@inbox.ru>
2023-10-25 17:26:19 +0300

    first commit

---
+++ src/components/VideoList.jsx
@@ -0,0 +1,33 @@
+const videos = [
+{
+    name: 'Замыкания от и до',
+    duration:15,
+    id: 1
+},
+{
+    name: 'Роадмеп',
+    duration:15,
+    id: 2
+},
+{
+    name: 'JS JSX',
+    duration:15,
+    id: 3
+}
+]
+export function VideoList(){
+    return(
+        <>
+          <h1>Video List</h1>
+            {videos.map((video)=> {
+                return(
+                    <div className="listItem" key={video.id}>
+                        <p>{video.name}</p>
+                        <p>{video.duration}</p>
+                        <button>like</button>
+                    </div>
+                )
+            })}
+        </>
+    )
+}
